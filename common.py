73bc3b821d13b322523303b69775d873fd3da4ec
David Eilering <deilering@riotgames.com>
2014-04-09 16:51:10 -0500

    fixing DELETE for django 1.6

--- common.py
+++ common.py
@@ -112,8 +112,7 @@ def getPutData(request):
     :type request: Request
     """
     dataDict = {}
-    data = getattr(request.raw_post_data, request.body)
-    for n in urlparse.parse_qsl(data):
+    for n in urlparse.parse_qsl(request.body):
         dataDict[n[0]] = n[1]
 
     setattr(request, 'PUT', dataDict)
